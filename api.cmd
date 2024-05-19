4c484b020f1a5695f5572e77b30332d40e8b8fde
Carlos Ramos <57056947+CarlosIRamosV@users.noreply.github.com>
2024-05-19 15:06:04 -0600

    fix

--- api.cmd
+++ api.cmd
@@ -1,11 +1,12 @@
 @echo off
 title API
 
+cd %~dp0
+
 REM check if exist .env file
 if not exist .env (
     echo DATABASE_URL=mysql://admin:admin@localhost:3306/redHatDataGridVideo > .env
 )
 
 REM Run the application
-cd bin
-films.exe
+.\bin\films.exe

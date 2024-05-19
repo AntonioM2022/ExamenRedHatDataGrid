544827f2a1e01b6c1bb34163a8750e30a693267d
Carlos Ramos <57056947+CarlosIRamosV@users.noreply.github.com>
2024-05-19 13:58:43 -0600

    fix

--- api.cmd
+++ api.cmd
@@ -3,7 +3,7 @@ title API
 
 REM check if the DATABASE_URL is set in the environment
 if not defined DATABASE_URL (
-  setx DATABASE_URL "mysql://admin:admin@database:3306/redHatDataGridVideo" /M
+  setx DATABASE_URL "mysql://admin:admin@localhost:3306/redHatDataGridVideo" /M
 )
 
 REM Run the application
